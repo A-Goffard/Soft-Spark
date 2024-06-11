4112c58270985a52e888618be98702788f3b13a4
jhonsdb <davilajhon08@gmail.com>
2024-06-11 12:02:17 +0200

    creacion componente TrainingCard

--- frontend/src/views/TrainingView.vue
+++ frontend/src/views/TrainingView.vue
@@ -2,21 +2,24 @@
   <div class="general">
     <div class="main">
       <h1>Training</h1>
-      <!-- <Pedagogia /> -->
-      <div class="cards">
-        <Kit/>
-        <ToolCardsTrainSoftSkills/>
-        <CertificatesTrainees/>
-        <AttendanceSheet/>
-        <SurveyTrainees/>
-        <Techniques/>
-        <SSTTrainers/>
-        <CertificatesTrainers/>
-        <SurveyTrainers/>
-        <TTWBL/>
-        <THWBL/>
+      
+      <div class="wrapper">
+        <DestinationCard
+          v-for="(destination, index) in destinations"
+          :key="index"
+          :id="'c' + index"
+          :image-id="'i' + index"
+          :story-id="'s' + index"
+          :title="destination.title"
+          :description="destination.description"
+          :image-url="destination.imageUrl"
+          @click="changeActiveComponent(destination.componentName)"
+        />
       </div>
       
+      <div class="cards">
+        <component :is="getComponent(activeComponent)" />
+      </div>
     </div>
   
 
@@ -28,10 +31,31 @@
 .general {
   padding-top: 5rem;
 }
+
+.cards {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+}
+
+.wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  max-width: 1200px;
+  margin-top: 20px;
+  perspective: 1000px;
+}
 </style>
-../components/PedagogiaCard.vue
+
 <script setup>
-/* import Pedagogia from '../components/PedagogiaCard.vue'; */
+
+import { ref } from 'vue';
+import DestinationCard from '../components/DestinationCard.vue';
+
 import Kit from '../components/training/Kit.vue';
 import ToolCardsTrainSoftSkills from '../components/training/ToolCardsTrainSoftSkills.vue';
 import CertificatesTrainees from '../components/training/CertificatesTrainees.vue';
@@ -43,4 +67,43 @@ import CertificatesTrainers from '../components/training/CertificatesTrainers.vu
 import SurveyTrainers from '../components/training/SurveyTrainers.vue';
 import TTWBL from '../components/training/TTWBL.vue';
 import THWBL from '../components/training/THWBL.vue';
- </script>
+
+const destinations = ref([
+{ title: 'Kit', description: 'Target 1 description', imageUrl: '/1.png', componentName: 'Kit' },
+  { title: 'ToolCardsTrainSoftSkills', description: 'Target 1 description', imageUrl: '/1.png', componentName: 'ToolCardsTrainSoftSkills' },
+  { title: 'CertificatesTrainees', description: 'Target 2 description', imageUrl: '/2.png', componentName: 'CertificatesTrainees' },
+  { title: 'AttendanceSheet', description: 'Target 3 description', imageUrl: '/3.png', componentName: 'AttendanceSheet' },
+  { title: 'SurveyTrainees', description: 'Target 4 description', imageUrl: '/4.png', componentName: 'SurveyTrainees' },
+  { title: 'Techniques', description: 'Target 5 description', imageUrl: '/5.png', componentName: 'Techniques' },
+  { title: 'SSTTrainers', description: 'Target 6 description', imageUrl: '/6.png', componentName: 'SSTTrainers' },
+  { title: 'CertificatesTrainers', description: 'Target 7 description', imageUrl: '/7.png', componentName: 'CertificatesTrainers' },
+  { title: 'SurveyTrainers', description: 'Target 8 description', imageUrl: '/8.png', componentName: 'SurveyTrainers' },
+  { title: 'TTWBL', description: 'Target 8 description', imageUrl: '/8.png', componentName: 'TTWBL'},
+  { title: 'THWBL', description: 'Target 8 description', imageUrl: '/8.png', componentName: 'THWBL'}
+]);
+
+const activeComponent = ref('');
+
+const changeActiveComponent = (componentName) => {
+  activeComponent.value = componentName;
+};
+
+const componentMap = {
+  Kit,
+  ToolCardsTrainSoftSkills,
+  CertificatesTrainees,
+  AttendanceSheet,
+  SurveyTrainees,
+  Techniques,
+  SSTTrainers,
+  CertificatesTrainers,
+  SurveyTrainers,
+  TTWBL,
+  THWBL
+};
+
+const getComponent = (name) => {
+  return componentMap[name] || null;
+};
+</script>
+
